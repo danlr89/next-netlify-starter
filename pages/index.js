27945d293913ec3910d36ab650f2b7c37093f4a1
danlr89 <131617602+danlr89@users.noreply.github.com>
2023-04-23 23:25:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,16 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
+   <body>
+    <script type="module">
+  import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.0/dist/web.js';
+
+  Typebot.initStandard({ typebot: "my-typebot-r4d95pe" });
+</script>
+
+<typebot-standard style="width: 100%; height: 600px; "></typebot-standard>
+  </body>
+
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
